ea0d94de973b04ecaed7c44cc99c4589b751297b
tangyunxiao <tangyunxiao@chongaitianxia.cn>
2019-07-27 15:05:01 +0800

    feat: 添加营销组件示例

---
+++ src/page/home/home.js
@@ -0,0 +1,21 @@
+// 此文件是由模板文件 ".dtpl/page/home.js.dtpl" 生成的，你可以自行修改模板
+
+// import { test } from './services/index';
+import { throttle } from '/utils/util';
+import create from '/utils/create';
+import store from '/page/globalStore';
+import 'minapp';
+
+create.Page({
+  store,
+  data: {
+    list: [
+      {
+        id: 'market',
+        sub: 'market',
+        name: '营销组件',
+        url: 'https://ranshaw.oss-cn-shanghai.aliyuncs.com/example/market.png',
+      },
+    ],
+  },
+});
